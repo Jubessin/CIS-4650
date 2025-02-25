20208866e4838fa917c2ce09364da1eda5215735
Jubessin <justinerogun@hotmail.com>
2025-02-25 16:13:37 -0500

    - update explist class to extend absyn class

--- absyn/ExpList.java
+++ absyn/ExpList.java
@@ -1,11 +1,15 @@
 package absyn;
 
-public class ExpList {
+public class ExpList extends Absyn {
     public Exp head;
     public Exp tail;
 
     public ExpList(Exp head, Exp tail) {
         this.head = head;
         this.tail = tail;
+    }
+
+    public void accept(AbsynVisitor visitor, int level) {
+        visitor.visit(this, level);
     }
 }
