13bab80fa1ae470244c4b239f21fe7c6fff507ca
Jubessin <justinerogun@hotmail.com>
2025-02-25 15:12:20 -0500

    - create boolean expression - create visitor method for boolean expression

--- absyn/AbsynVisitor.java
+++ absyn/AbsynVisitor.java
@@ -5,6 +5,7 @@ public interface AbsynVisitor {
     public void visit(IntExp exp, int level);
     public void visit(NameTy exp, int level);
     public void visit(NilExp exp, int level);
+    public void visit(BoolExp exp, int level);
     public void visit(DecList exp, int level);
     public void visit(SimpleDec exp, int level);
 }
