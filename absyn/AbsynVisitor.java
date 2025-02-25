206369754322ca84cd316300a354acbbdc439a44
Jubessin <justinerogun@hotmail.com>
2025-02-25 14:44:36 -0500

    - create nil expression class - create method for visiting nil expression

--- absyn/AbsynVisitor.java
+++ absyn/AbsynVisitor.java
@@ -1,8 +1,9 @@
 package absyn;
 
 public interface AbsynVisitor {
-    public void visit(NameTy exp, int level);
     public void visit(Dec exp, int level);
+    public void visit(NameTy exp, int level);
+    public void visit(NilExp exp, int level);
     public void visit(DecList exp, int level);
     public void visit(SimpleDec exp, int level);
 }
