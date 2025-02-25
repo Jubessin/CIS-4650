72f1a8ba5f273ab5e9367da887837b0521321238
Jubessin <justinerogun@hotmail.com>
2025-02-25 17:06:54 -0500

    - cleanup

--- absyn/AbsynVisitor.java
+++ absyn/AbsynVisitor.java
@@ -3,18 +3,21 @@ package absyn;
 public interface AbsynVisitor {
     public void visit(Dec dec, int level);
     public void visit(SimpleDec dec, int level);
+
     public void visit(OpExp exp, int level);
     public void visit(IntExp exp, int level);
     public void visit(NilExp exp, int level);
     public void visit(VarExp exp, int level);
     public void visit(BoolExp exp, int level);
     public void visit(AssignExp exp, int level);
+    public void visit(FunctionDec exp, int level);
 
     public void visit(NameTy type, int level);
+
     public void visit(DecList list, int level);
     public void visit(ExpList list, int level);
-    public void visit(VarDecList exp, int level);
+    public void visit(VarDecList list, int level);
+    
     public void visit(IndexVar var, int level);
     public void visit(SimpleVar var, int level);
-    public void visit(FunctionDec exp, int level);
 }
