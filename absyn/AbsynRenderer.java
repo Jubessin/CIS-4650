754bce9586ecf47007de072f16039f607ea84072
Jubessin <justinerogun@hotmail.com>
2025-03-01 13:24:47 -0500

    - implement declaration list rendering

--- absyn/AbsynRenderer.java
+++ absyn/AbsynRenderer.java
@@ -115,7 +115,10 @@ public class AbsynRenderer implements AbsynVisitor {
     }
 
     public void visit(DecList list, int level) {
-
+        while (list != null) {
+            list.head.accept(this, level);
+            list = list.tail;
+        }
     }
 
     public void visit(ExpList list, int level) {
