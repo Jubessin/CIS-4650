8f9b7fd81932adfa65870d6a22a7033d74d2029a
Jubessin <justinerogun@hotmail.com>
2025-03-01 14:42:41 -0500

    - cleanup

--- absyn/CompoundExp.java
+++ absyn/CompoundExp.java
@@ -1,8 +1,8 @@
 package absyn;
 
 public class CompoundExp extends Exp {
-    public VarDecList decs;
     public ExpList exps;
+    public VarDecList decs;
 
     public CompoundExp(int row, int col, VarDecList decs, ExpList exps) {
         super(row, col);
