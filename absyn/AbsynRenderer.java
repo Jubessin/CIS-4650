00de9175c5d1436c9d367d0c8e368eb12c9b7408
Jubessin <justinerogun@hotmail.com>
2025-03-01 12:16:04 -0500

    - create renderer for absyn - update main file

---
+++ absyn/AbsynRenderer.java
@@ -0,0 +1,89 @@
+package absyn;
+
+public class AbsynRenderer implements AbsynVisitor {
+    private static final int INDENT = 4;
+    
+    private static void indent(int level) {
+
+    }
+    
+    public void visit(Dec dec, int level) {
+
+    }
+
+    public void visit(SimpleDec dec, int level) {
+
+    }
+
+    public void visit(FunctionDec dec, int level) {
+
+    }
+
+    public void visit(IfExp exp, int level) {
+
+    }
+
+    public void visit(OpExp exp, int level) {
+
+    }
+
+    public void visit(IntExp exp, int level) {
+
+    }
+
+    public void visit(NilExp exp, int level) {
+
+    }
+
+    public void visit(VarExp exp, int level) {
+
+    }
+
+    public void visit(BoolExp exp, int level) {
+
+    }
+
+    public void visit(WhileExp exp, int level) {
+
+    }
+
+    public void visit(AssignExp exp, int level) {
+
+    }
+
+    public void visit(ReturnExp exp, int level) {
+
+    }
+
+    public void visit(CompoundExp exp, int level) {
+
+    }
+
+    public void visit(CallExp exp, int level) {
+
+    }
+
+    public void visit(NameTy type, int level) {
+
+    }
+
+    public void visit(DecList list, int level) {
+
+    }
+
+    public void visit(ExpList list, int level) {
+
+    }
+
+    public void visit(VarDecList list, int level) {
+
+    }
+
+    public void visit(IndexVar var, int level) {
+
+    }
+
+    public void visit(SimpleVar var, int level) {
+        
+    }
+}
