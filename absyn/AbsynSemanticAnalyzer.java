34c94da1ddcfdd4dfa2b9e8de8af75897e8017c9
Jubessin <justinerogun@hotmail.com>
2025-03-09 20:00:13 -0400

    - create utility functions for semantic analyzer

--- absyn/AbsynSemanticAnalyzer.java
+++ absyn/AbsynSemanticAnalyzer.java
@@ -2,24 +2,50 @@ package absyn;
 
 import java.util.*;
 
+// TODO: add dummy types for int and bool for operation expression comparisons  
 public class AbsynSemanticAnalyzer implements AbsynVisitor {
     private static final HashMap<String, ArrayList<NodeType>> table = new HashMap<String, ArrayList<NodeType>>();
-    private static final NodeType dummyBool = new NodeType(null, new BoolExp(0, 0, false), -1);
-    private static final NodeType dummyInt = new NodeType(null, new IntExp(0, 0, 0), -1);
     private static final StringBuilder sb = new StringBuilder();
 
     private static int scope;
 
     private boolean isValid = true;
     
-    private static addNode(Dec dec) {
-        var list = table.getOrDefault(dec.name);
+    private static void insert(Dec dec) {
+        var list = table.getOrDefault(dec.name, null);
 
         if (list == null) {
             list = new ArrayList<NodeType>();
         }
 
         list.add(new NodeType(dec.name, dec, scope));
+    }
+
+    private static NodeType lookup(String id) {
+        var list = table.getOrDefault(id, null);
+
+        if (list == null)
+            return null;
+
+        var size = list.size();
+
+        if (size == 0)
+            return null;
+
+        return list.get(size - 1);
+    }
+
+    private static void delete() {
+        var iter = table.entrySet().iterator();
+
+        while (iter.hasNext()) {
+            var pair = (Map.Entry)iter.next();
+            var key = pair.getKey();
+            var value = (ArrayList)pair.getValue();
+
+            // TODO: Test that this makes sense.
+            value.removeIf(n -> ((NodeType)n).level > scope);
+        }
     }
     
     public boolean finish(String file) {
@@ -31,35 +57,35 @@ public class AbsynSemanticAnalyzer implements AbsynVisitor {
     }
 
     public void visit(SimpleDec dec, int level) {
-        addNode(dec);
+        insert(dec);
     }
 
     public void visit(ArrayDec dec, int level) {
-        addNode(dec);
+        insert(dec);
     }
 
     public void visit(FunctionDec dec, int level) {
-        addNode(dec);
+        insert(dec);
 
-        visit(dec.params, level);
+        // visit(dec.params, level);
 
-        ++scope;
+        // ++scope;
 
-        visit(dec.body, level);
+        // visit(dec.body, level);
     }
 
     public void visit(IfExp exp, int level) {
-        visit(exp.test, level);
+        // visit(exp.test, level);
         
-        ++scope;
+        // ++scope;
 
-        visit(exp.body, level);
+        // visit(exp.body, level);
         
-        if (exp._else != null) {
-            visit(exp._else);
-        }
+        // if (exp._else != null) {
+        //     visit(exp._else, level);
+        // }
 
-        --scope;
+        // --scope;
     }
 
     public void visit(OpExp exp, int level) {
